0bab02dfb2e2491dbb9eb15db3fcd6ed457629c1
LutzGe <gabriel.lutz5@gmail.com>
2020-10-31 01:14:01 -0300

    changes

--- app/src/Models/CountPimba.php
+++ app/src/Models/CountPimba.php
@@ -1,9 +1,11 @@
 <?php
 
-// require '../Classes/RegraDoTres.php';
-// require '../Classes/RegraDoCinco.php';
-// require '../Classes/RegraDoPimba.php';
-// require '../Classes/Replacer.php';
+echo getcwd();
+
+require_once getcwd().'/src/Classes/RegraDoTres.php';
+require_once getcwd().'/src/Classes/RegraDoCinco.php';
+require_once getcwd().'/src/Classes/RegraDoPimba.php';
+require_once getcwd().'/src/Classes/Replacer.php';
 
 $regraTres = new RegraDoTres();
 $regraCinco = new RegraDoCinco();
